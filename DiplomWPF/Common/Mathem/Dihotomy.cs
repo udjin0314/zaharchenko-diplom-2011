b92265925843f62e2795a13eea7261e7e9aa5c37
udjin0314@gmail.com <udjin0314@gmail.com@9b1ecc3e-69dd-7a57-c31a-9e836f8c8048>
2010-12-15 21:52:57 +0000

    Math Functions

---
+++ DiplomWPF/Common/Mathem/Dihotomy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DiplomWPF.Common.Mathem.Functions;
+
+namespace DiplomWPF.Common.Mathem
+{
+    public class Dihotomy
+    {
+        private double epsilon = 1e-8;
+        private Function func;
+
+        public Dihotomy(Function func)
+        {
+            this.func = func;
+        }
+        public Dihotomy(Function func, double epsilon)
+        {
+            this.func = func;
+            this.epsilon = epsilon;
+        }
+
+        public double resolve(double a, double b)
+        {
+            double c;
+            //if (func.resolve(a) * func.resolve(b) > 0) 
+            while (b - a > epsilon)
+            {
+                c = (a + b) / 2;
+                if (func.resolve(b) * func.resolve(c) < 0)
+                    a = c;
+                else
+                    b = c;
+            }
+           return (a + b) / 2;
+        }
+    }
+}
---
+++ DiplomWPF/Common/Mathem/Functions/MkrFunction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiplomWPF.Common.Mathem.Functions
+{
+    public class MkrFunction:Function
+    {
+        double alphar;
+        double K;
+        
+        public double resolve(double param)
+        {
+            return MathHelper.bessel1(param) + alphar / K * MathHelper.bessel0(param); 
+        }
+    }
+}
